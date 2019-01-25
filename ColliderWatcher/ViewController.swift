fff1cca86bef3373262d8d09c03c8b9c75ebc859
Артемий Шлесберг <temitrix@gmail>
2019-01-25 16:08:31 +0300

    Initial Commit

---
+++ ColliderWatcher/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColliderWatcher
+//
+//  Created by Артeмий Шлесберг on 25/01/2019.
+//  Copyright © 2019 Collider. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
